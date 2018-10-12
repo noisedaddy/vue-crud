145571b997e3ee5026895f1e63f9974a6c9de799
Veljko <veljko.radenkovic@gmail.com>
2018-10-12 14:43:18 +0200

    users finish

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <test msg="hello from app.vue test component"></test>
+    <!-- <test msg="hello from app.vue test component"></test> -->
+    <user></user>
   </div>
 </template>
 
 <script>
-import Test from './components/Test'
+// import Test from './components/Test'
+import User from './components/Users'
 
 export default {
   name: 'App',
   components: {
-    Test
+    User
   }
 }
 </script>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,15 +2,18 @@
     <div id="users">
         <h1>Users:</h1>
         <form v-on:submit="addUser">
-            <input type="text" v-model="newUser.name" placeholder="Enter name">
+            <input type="text" v-model="newUser.name" placeholder="Enter name"/>
             <br />
-            <input type="text" v-model="newUser.email" placeholder="Enter email">
+            <input type="text" v-model="newUser.email" placeholder="Enter email"/>
             <br />
-            <input type="button" value="Submit">
-        </form>        
+            <input type="submit" value="Submit"/>
+        </form>
         <ul>
             <li v-for="user in users">
-                {{user.name}}:{{user.email}}
+              <input type="checkbox" class="toggle" v-model="user.contacted" />
+                <span :class="{contacted: user.contacted}">
+                  {{user.name}}:{{user.email}} <button v-on:click="deleteUser(user)">X</button>
+                </span>
             </li>
         </ul>
     </div>
@@ -47,26 +50,25 @@ export default {
     },
     methods: {
         addUser: function(e){
-            console.log('addd user');
             e.preventDefault();
-            
+            console.log(this.users.name);
+
             this.users.push({
-                name: this.users.name,
-                email: this.users.email,
+                name: this.newUser.name,
+                email: this.newUser.email,
                 contacted: false
             })
+        },
+        deleteUser: function(user){
+          this.users.splice(this.users.indexOf(user), 1);
         }
     }
+
 }
 </script>
 
 <style scoped>
-#test {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .contacted {
+    text-decoration: line-through;
+  }
 </style>
